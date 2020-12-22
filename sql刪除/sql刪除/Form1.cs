b45f4a79187c505c0cc2979e96361c2f0208aa99
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-22 17:29:46 +0800

    Add files via upload

---
+++ sql刪除/sql刪除/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sql刪除
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using(SqlConnection con = cn1.cn)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText =
+                    $"Delete From Account Where AccountID = (N'38')";
+                //$"Delete From Account Where Name = (N'juje')";
+                //$"Delete * From Account 或 $"Delete From Account
+                //刪除某資料表中所有的資料
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ sql刪除/sql刪除/cn1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sql刪除
+{
+    public static class cn1
+    {
+        public static SqlConnection cn
+        {
+            get
+            {
+                return new SqlConnection("Data Source=.;Initial Catalog=Disneyland_Shanghai_ANO;Integrated Security=True");
+            }
+        }
+    }
+}
